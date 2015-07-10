c4b49856fd02f2b0fc71d033f7cfecdd54e4fae7
Markus Kauppila <markus.kauppila@gmail.com>
2015-07-10 10:16:22 +0300

    Disable macro semicolon appender

--- format-objc-file.sh
+++ format-objc-file.sh
@@ -22,7 +22,7 @@ python "$DIR"/custom/LiteralSymbolSpacer.py "$1"
 # The formatter gets confused by C++ inline constructors that are broken onto multiple lines
 python "$DIR"/custom/InlineConstructorOnSingleLine.py "$1"
 # Add a semicolon at the end of simple macros
-python "$DIR"/custom/MacroSemicolonAppender.py "$1"
+#python "$DIR"/custom/MacroSemicolonAppender.py "$1"
 # Add an extra newline before @implementation and @interface
 python "$DIR"/custom/DoubleNewlineInserter.py "$1"
 
